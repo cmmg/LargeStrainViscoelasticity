e68fc2bd632246c031f38b1d1c98275a896cefdb
Sudip Kunda <skunda@wisc.edu>
2024-07-04 07:09:11 -0500

    Basic compilation test with deal.II libraries.
    Here, main.cc does nothing except print "hello world" to the console.
    But some of the libraries included at the top of the main.cc file are
    from deal.II - just enough to create a grid and output data about the
    grid to a file.

---
+++ main.cc
@@ -0,0 +1,14 @@
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+int main() {
+
+    std::cout << "hello world" << std::endl;
+
+    return 0;
+}
