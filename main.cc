a5a4bb20b9b95ff44ed6be69c5fb1745c29c6295
Sudip Kunda <skunda@wisc.edu>
2024-07-19 20:30:35 -0500

    Start wrapping up code in a class structure.
    dim was removed from parameters.h because it was not being preprocessed
    as I intended.
    
    The code creates a triangulation with 64 cells and 125 vertices inside a
    3D cube.

--- main.cc
+++ main.cc
@@ -10,33 +10,62 @@
 
 using namespace dealii;
 
+template <int dim>
+class Problem {
+    public:
+        Problem();
+        void run();
+
+    private:
+        void setup_system();
+
+    private:
+        Triangulation<dim> triangulation;
+};
+
+template <int dim>
+Problem<dim>::Problem () {
+    GridGenerator::hyper_cube(triangulation);
+}
+
+template <int dim>
+void Problem<dim>::run () {
+    setup_system();
+}
+
+template <int dim>
+void Problem<dim>::setup_system () {
+    std::cout << "Setting up" << std::endl;
+
+    triangulation.refine_global(2);
+
+    std::cout << "No of cells : " << triangulation.n_active_cells() << std::endl;
+    std::cout << "No of vertices : " << triangulation.n_vertices() << std::endl;
+}
+
 int main() {
 
     std::cout << "\n-- Start\n" << std::endl;
 
-    // Create an object that will contain the initial mesh. The dim parameter
-    // decides whether it is 2D or 3D
-    Triangulation<dim> triangulation;
+    Problem<3> problem;
 
-    GridGenerator::hyper_cube(triangulation);
-    triangulation.refine_global(4);
+    problem.run();
 
-    DoFHandler<dim> dof_handler(triangulation);
-    FE_Q<dim> fe(1);
-
-    dof_handler.distribute_dofs(fe);
-
-    DataOut<dim> data_out;
-
-    data_out.attach_dof_handler(dof_handler);
-
-    std::ofstream output_file("solution.vtu");
-
+    /*DoFHandler<dim> dof_handler;*/
+    /*FE_Q<dim> fe(1);*/
+    /**/
+    /**/
+    /*// Setup system*/
+    /*dof_handler.initialize(triangulation, fe);*/
+    /*dof_handler.distribute_dofs(fe);*/
+    /**/
+    /*DataOut<dim> data_out;*/
+    /*data_out.attach_dof_handler(dof_handler);*/
+    /*std::ofstream output_file("solution.vtu");*/
     /*data_out.build_patches();*/
+    /*data_out.write_vtu (output_file);*/
 
-    data_out.write_vtu (output_file);
-
-    std::cout << "\n-- End\n" << std::endl;
+    std::cout << "\n-- End" << std::endl;
 
     return 0;
 
