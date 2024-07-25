0ec78572fa75b502451b8aff6653a0b3e7d56b7d
Sudip Kunda <skunda@wisc.edu>
2024-07-24 23:11:42 -0500

    Solution vector and system RHS added and initialized

--- main.cc
+++ main.cc
@@ -18,15 +18,17 @@ class Problem {
         Problem();
         void run();
 
-    private:
+    private: // functions
         void setup_system();
         void output_results();
 
-    private:
+    private: // variables
         Triangulation<dim> triangulation;
         DoFHandler<dim> dof_handler;
         DataOut<dim> data_out;
         FESystem<dim> fe;
+        Vector<double> system_rhs;
+        Vector<double> solution;
 };
 
 template <int dim>
@@ -43,19 +45,19 @@ void Problem<dim>::run () {
 
 template <int dim>
 void Problem<dim>::setup_system () {
-    std::cout << "Setting up" << std::endl;
+    std::cout << "-- Setting up\n" << std::endl;
 
     GridGenerator::hyper_cube(triangulation);
-    /*triangulation.refine_global(1);*/
+    triangulation.refine_global(1);
+    dof_handler.distribute_dofs(fe);
+    solution.reinit(dof_handler.n_dofs());
+    system_rhs.reinit(dof_handler.n_dofs());
 
     std::cout << "No of cells : " << triangulation.n_active_cells() << std::endl;
     std::cout << "No of vertices : " << triangulation.n_vertices() << std::endl;
-
-    dof_handler.distribute_dofs(fe);
-
     std::cout << "No of dofs : " << dof_handler.n_dofs() << std::endl;
 
-    std::cout << "Set up complete" << std::endl;
+    std::cout << "\n-- Set up complete" << std::endl;
 }
 
 template <int dim>
@@ -66,13 +68,13 @@ void Problem<dim>::output_results () {
     data_out.build_patches();
     data_out.write_vtu (output_file);
 
-    std::cout << "Results written" << std::endl;
+    std::cout << "\nResults written" << std::endl;
 
 }
 
 int main() {
 
-    std::cout << "\n-- Simulation started\n" << std::endl;
+    std::cout << "\n---- Simulation started\n" << std::endl;
 
     // Create the problem
     Problem<3> problem;
@@ -80,7 +82,7 @@ int main() {
     // Solve the problem
     problem.run();
 
-    std::cout << "\n-- Simulation ended" << std::endl;
+    std::cout << "\n---- Simulation ended" << std::endl;
 
     return 0;
 
