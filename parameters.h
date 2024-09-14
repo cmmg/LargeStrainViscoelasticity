163b152ccf91141a3f45a6ad228d8a2ca38659ae
Sudip Kunda <skunda@wisc.edu>
2024-09-14 14:43:40 -0500

    Add viscoelasticity material parameters to parameters.h file

--- parameters.h
+++ parameters.h
@@ -1,3 +1,13 @@
 #define Y 200        // Young's modulus
 #define nu 0.33      // Poisson's ratio
 #define top_surface_speed 1      // Speed of the top surface
+
+// Material parameters
+#define mu_0 20 // Shear modulus [Pa]
+#define lambda_L 1.09 // Maximum stretch [-]
+#define K 800 // Bulk modulus [Pa]
+#define sigma_0 25 // Strength parameter for viscous stretch rate
+#define np 3 // Exponent for viscous stretch rate
+#define G_0 4500 // Elastic modulus for element C
+#define G_infinity 600 // Elastic modulus for element E
+#define eta 60000 // Viscosity for element D
